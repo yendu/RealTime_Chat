7731d2829e335691ca5bc7e0ed74dfe44005f9a8
yendu <yendu669@gmail.com>
2021-06-17 01:54:58 +0300

    socket io implemented

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const httpServer=require('http')
-const io=require('socket.io')
+const {Server}=require('socket.io')
+const userController=require('./controllers/userController')
+const messageController=require('./controllers/messageController')
+
 
 dotenv.config({path:'./config.env'});
 const app=require('./app');
+const { Socket } = require('dgram');
 
 
 
@@ -18,7 +22,30 @@ mongoose.connect(process.env.DATABASE_LOCAL,{
 //    console.log("working");
 // });
 
-app.listen(process.env.PORT,()=>{
+// app.listen(process.env.PORT,()=>{
     
-    console.log("Server started ");
-});
+//     console.log("Server started ");
+// });
+const server=httpServer.createServer(app)
+const io=new Server(server)
+
+io.on('connection',(socket)=>{
+    console.log("socket started")
+    socket.on('message',(msg)=>{
+        console.log(msg);
+        socket.in(msg).emit('data',msg);
+        //filter the message
+        //create message
+    })
+    socket.on('room',async(roomid)=>{
+        console.log(roomid)
+        socket.join(roomid);
+        const users= await userController.findAllUsers();
+        socket.emit(roomid+"users",users);
+        
+        
+    })
+})
+server.listen(process.env.PORT,()=>{
+    console.log("Server Started");
+})
