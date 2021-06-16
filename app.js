3914324254c65973eb1f125209406e86d1ca3c72
yendu <yendu669@gmail.com>
2021-06-17 01:57:06 +0300

    minor change on app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express=require('express')
+const path=require('path')
 const userRouter=require('./routes/userRoutes')
+const messageRouter=require('./routes/messageRoutes')
 const app=express();
 const globalErrorHandler = require('./controllers/errorControllers');
 const AppError = require('./utils/appError');
 
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json({ limit: '10kb' })); 
 app.use('/v1/user',userRouter);
+app.use('/v1/message',messageRouter)
 
 app.all('*',(req,res,next)=>{
    next(new AppError('Cant find the url',404));
